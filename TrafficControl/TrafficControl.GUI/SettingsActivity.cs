1c00d4f6bcc11ed08ebfb2beca64b728ac90e5b8
jacobboes <jacobboeseriksen@hotmail.com>
2016-04-28 11:29:14 +0200

    update

--- TrafficControl/TrafficControl.GUI/SettingsActivity.cs
+++ TrafficControl/TrafficControl.GUI/SettingsActivity.cs
@@ -1,19 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Preferences;
-using Android.Runtime;
-using Android.Support.V4.App;
-using Android.Support.V4.Widget;
 using Android.Views;
-using Android.Widget;
-using TrafficControl.BLL;
-using TrafficControl.GUI.Menu;
 using TrafficControl.GUI.Settings;
 
 namespace TrafficControl.GUI
